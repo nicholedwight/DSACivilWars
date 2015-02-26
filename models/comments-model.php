0a96b7493251b4062366bd0104a6c10501c1b033
Nichole Dwight <nicholedwight@gmail.com>
2015-02-26 19:47:59 +0000

    removing old connectToDb function

--- models/comments-model.php
+++ models/comments-model.php
@@ -16,19 +16,6 @@ class Comments {
    * @return [array]
    */
 
-//    function connectToDatabase() {
-//  if ($_SERVER['SERVER_NAME'] == 'twitter.dev') {
-//    $db = new mysqli('localhost', 'root', 'root', 'civil_war');
-//  }
-//  else {
-//    $db = new mysqli('mysql5.cems.uwe.ac.uk', 'fet13000673', '8LYn8K', 'fet13000673');
-//  }
-//  if ($db->connect_errno > 0) {
-//    die('Unable to connect to database [' . $db->connect_error . ']');
-//  }
-//  else return $db;
-// }
-
   //   public function insertComment() {
   //   $db = connectToDatabase();
   //   $comment = $_POST['comment'];
