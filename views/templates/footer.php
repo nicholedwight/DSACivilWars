33262defaf76e00905172707a946746df4e18e02
Nichole Dwight <nicholedwight@gmail.com>
2015-03-01 15:13:03 +0000

    changing color scheme and adding footer

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,4 +1,7 @@
 </div>
+<footer>
+  <p> testest</p>
+</footer>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
